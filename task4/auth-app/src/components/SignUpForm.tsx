56258e12855ba04ca33c82ad9f95dd1919a6b9f2
Jakhongir <jahongirnagmatov@gmail.com>
2023-10-11 16:23:33 +0500

    very initial form in client side

--- task4/auth-app/src/components/SignUpForm.tsx
+++ task4/auth-app/src/components/SignUpForm.tsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface SignUpFormState{
+    name: string;
+    email: string;
+}
 
 const SignUpForm = () => {
+
+    const [formData, setFormData] = useState<SignUpFormState>({
+        name: '',
+        email: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormData(prevData => ({...prevData, [name]: value}))
+    }
+
+    const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formData);
+        
+        
+        try {
+            const response = axios.post('http://localhost:8000/api/v1/signup', formData)
+            console.log(response);
+            
+        } catch (error){
+            console.error(error)
+        }
+        
+    }
+
   return (
-    <div>
-        Hi universe!
-    </div>
+    <>
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="name" onChange={handleChange} value={formData.name} required maxLength={30}/>
+
+            <input type="email" name="email" onChange={handleChange} value={formData.email} required maxLength={50}/>
+
+            <button type="submit" >Submit</button>
+        </form>
+    </>
   )
 }
 
